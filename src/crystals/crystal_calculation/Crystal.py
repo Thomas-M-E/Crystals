b18383e602a719aab2153e8fd7270d6d8663bb2c
Thomas Everard <thomas.everard@outlook.com>
2022-04-27 23:55:25 +0100

    004: Slight refactor

--- src/crystals/crystal_calculation/Crystal.py
+++ src/crystals/crystal_calculation/Crystal.py
@@ -1,12 +1,13 @@
 import numpy as np
 import random
 from typing import Tuple
+import logging
 
 # TODO: set up a canvas class for storing the info of the crystal
 # TODO: finish the run() method
 
 __DIR_CHOICE = np.array([[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,-1],[1,-1],[-1,1]])
-
+LOGGER = logzero()
 class Crystal:
     """ Class for a crystal and it's growth
     """
@@ -63,10 +64,10 @@ def run():
                         c.crystal_loc.append([can.pos[0], can.pos[1]]) 
                         size = np.sqrt((can.pos[0] - c.mid_point)**2 + (pos[1]-c.mid_point)**2)
                         # Calculates how far from the centre the new particle on the edge of the crystal is
-                        if(size > radius - 1):# Checks if the crystal is too close to the injection circle
-                            radius = updateRadius(size) # Increases the size of the injection circle
-                            if(radius >= (N - 1 - c.mid_point)):# Ends the process if the crystal is too close to the edge
-                                print("Edge Reached")
+                        if(size > c.inject_radius - 1):# Checks if the crystal is too close to the injection circle
+                            c.inject_radius = c.update_radius(size) # Increases the size of the injection circle
+                            if(c.inject_radius >= (c.pixels - 1 - c.mid_point)):# Ends the process if the crystal is too close to the edge
+                                logging.info("Edge Reached")
                                 INJECT = False
 
                         WALK = False
@@ -74,34 +75,34 @@ def run():
                     
                 __DIREC = random.choice([0, 1, 2, 3, 4, 5, 6, 7])
                 # Steps for the random walk
-                pos[0] = pos[0] + __DIR_CHOICE[__DIREC,][0]
-                pos[1] = pos[1] + __DIR_CHOICE[__DIREC,][1]
+                can.pos[0] = can.pos[0] + __DIR_CHOICE[__DIREC,][0]
+                can.pos[1] = can.pos[1] + __DIR_CHOICE[__DIREC,][1]
 
                 #### Kills on the edge ####
-                if(pos[0] in {N-1, 0} or pos[1] in {N-1, 0}):
+                if(can.pos[0] in {N-1, 0} or can.pos[1] in {N-1, 0}):
                     WALK = False
                 #### Kill Circle ####
-                if(np.linalg.norm(pos - np.array([c.mid_point, c.mid_point])) > radius+5):
+                if(np.linalg.norm(can.pos - np.array([c.mid_point, c.mid_point])) > radius + 5):
                     killcount = killcount + 1
-                    WALK = False   #Breaks the loop for this random walk if it goes out
+                    WALK = False   # Breaks the loop for this random walk if it goes out
                                     #of bounds
             if(count > injections):
-                print("Injections completed")
-                INJECT = False #Ends the process if all the particles have been injected for this crystal
+                logging.info("Injections completed")
+                INJECT = False # Ends the process if all the particles have been injected for this crystal
             
         q = len(c.crystal_list)
         area = np.zeros(L)
         for k in range(L):
             for l in range(q):    
-                if(c.norm(crystalList[l][0]- c.mid_point, crystalList[l][1] - c.mid_point) < rseq[k]):
-                    area[k] = area[k]+1 #Counts the number of occupied sites within radius rseq[k]
+                if(c.norm(c.cryatal_list[l][0]- c.mid_point, c.cryatal_list[l][1] - c.mid_point) < rseq[k]):
+                    area[k] = area[k]+1 # Counts the number of occupied sites within radius rseq[k]
                 
                 
-        averageArea = averageArea + area #sums all the numbers of occupied sites for each crystal
+        averageArea = averageArea + area # sums all the numbers of occupied sites for each crystal
 
-    averageArea = averageArea/clusters #Averages the sums
-    end_time=time.time()
-    print('Elapsed time = ',repr(end_time-start_time))
+    averageArea = averageArea/clusters # Averages the sums
+    end_time = time.time()
+    print('Elapsed time = ', repr(end_time - start_time))
 
 if __name__ == __main__:
     run()
