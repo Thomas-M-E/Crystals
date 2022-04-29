f5217eefa87801286a1ea87092d6dd163cf641df
TMEverard <thomas.everard@outlook.com>
2022-04-30 00:30:30 +0100

    007: Refactor

---
+++ src/crystals/crystal_calculation/Canvas.py
@@ -0,0 +1,9 @@
+class Canvas:
+    def __init__(self, crystal: "Crystal", radius: int):
+        start = crystal.inject_circle()
+        self.pos = np.array([start[0], start[1]])
+        # TODO: pixels out of the crystal class or crystal created within the canvas
+        # TODO: make canvas a dataclass?
+        self.canvas = np.zeros(shape=(crystal.pixels, crystal.pixels))
+        self.canvas[crystal.mid_point, crystal.mid_point] = 1 # Sets the seed
+        
